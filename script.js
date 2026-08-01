228a57c6523af3c626dea88075a81b6acc8dfd06
vicktory17 <ochukovictory43@gmail.com>
2026-08-01 16:27:19 +0100

    Add files via upload
    Update portfolio website

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function greet() {
 function sayHello() {
     alert("Hello! Thanks for visiting my portfolio.");
 }
+
+function toggleMode() {
+    document.body.classList.toggle("dark-mode");
+}
+
+function sayHello() {
+    alert("Hello! Thanks for visiting my portfolio.");
+}
