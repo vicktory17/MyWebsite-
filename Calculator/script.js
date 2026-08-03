2a700a94ac9ce73f38b54691c05eb5d99883d5ba
vicktory17 <ochukovictory43@gmail.com>
2026-08-03 16:37:43 +0100

    Create script.js

---
+++ Calculator/script.js
@@ -0,0 +1,16 @@
+function greet() {
+    alert("Hello Victory! Welcome to JavaScript!");
+}
+
+function sayHello() {
+    alert("Hello! Thanks for visiting my portfolio.");
+}
+
+function toggleMode() {
+    document.body.classList.toggle("dark-mode");
+}
+
+function sayHello() {
+    alert("Hello! Thanks for visiting my portfolio.");
+}
+
